61603d34bf7a5378da477d9a27ed2cb3c7a0d8bb
Elen Alaverdyan <alaverdyanelen6@gmail.com>
2026-04-19 01:40:48 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import requests
 from datetime import datetime
 
-TOKEN = "քո_նոր_token-ը"
+TOKEN = "ՔՈ ՆՈՐ TOKENԸ"
 CHANNEL = "-1003756748417"
 
 report_date = datetime.now().strftime("%Y-%m-%d")
 
-text = f"📅 {report_date} \n🚨 Test message from GitHub bot"
+text = f"📅 {report_date}\n🚨 GitHub bot աշխատում է"
 
-r = requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    json={"chat_id": CHANNEL, "text": text}
-)
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-print(r.status_code, r.text)
+r = requests.post(url, json={
+    "chat_id": CHANNEL,
+    "text": text
+})
+
+print("STATUS:", r.status_code)
+print("RESPONSE:", r.text)
